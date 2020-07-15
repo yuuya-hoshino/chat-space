a97cad8205b8a918e513d05cb3203566d04dfe1a
yuuya-hoshino <65810632+yuuya-hoshino@users.noreply.github.com>
2020-07-15 17:51:55 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+=======
+  devise_for :users
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
+end
+>>>>>>> Stashed changes
