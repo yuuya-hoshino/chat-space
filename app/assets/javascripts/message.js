054cc5e1b7a9f3dea905b70a4f42371124c7bf08
yuuya-hoshino <65810632+yuuya-hoshino@users.noreply.github.com>
2020-07-15 17:46:08 +0900

    Create message.js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,69 @@
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="message">
+         <div class="upper-message">
+           <div class="upper-message__user-name">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="message">
+         <div class="upper-message">
+           <div class="upper-message__user-name">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+
+ 
+    $('.new-message').on('submit', function(e){ 
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+
+     $.ajax({
+       url: url,
+       type: "POST",
+       data: formData,
+       dataType: 'json',
+       processData: false,
+       contentType: false
+     })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $('form')[0].reset();
+        $('._btn').prop('disabled', false);
+      })
+      .fail(function() {
+        alert("メッセージ送信に失敗しました");
+      });
+    });
+
+});
