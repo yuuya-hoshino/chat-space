4db8748e5e7adbe4e1c86608c6eca65126f32bf5
yuuya-hoshino <65810632+yuuya-hoshino@users.noreply.github.com>
2020-07-31 18:42:05 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,42 +1,45 @@
 $(function(){
+  var last_message_id = $('.message:last').data("message-id");
   function buildHTML(message){
     if (message.image){
-      var html = `<div class="chat-main__message-lists__message" data-message-id=${message.id}>
-                    <div class="chat-main__message-lists__message__info">
-                      <div class="chat-main__message-lists__message__info__talker">
-                        ${message.user_name}
-                      </div>
-                      <div class="chat-main__message-lists__message__info__date">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                    <div class="chat-main__message-list__message__text">
-                      <div class="chat-main__message-list__message__text__content">
-                        ${message.content}
-                      </div>
-                      <img class="chat-main__message-list__message__text__image" src=${message.image}>
-                    </div>
-                  </div>`
-      return html;
-    } else {
-      var html = `<div class="chat-main__message-lists__message" data-message-id=${message.id}>
-                    <div class="chat-main__message-lists__message__info">
-                      <div class="chat-main__message-lists__message__info__talker">
-                        ${message.user_name}
-                      </div>
-                      <div class="chat-main__message-lists__message__info__date">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                    <div class="chat-main__message-list__message__text">
-                      <div class="chat-main__message-list__message__text__content">
-                        ${message.content}
-                      </div>
-                    </div>
-                  </div>`
-      return html;
-    };
-  }
+      var html =
+         `<div class="message" data-message-id=${message.id}>
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        var html =
+         `<div class="message" data-message-id=${message.id}>
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
 
   function formReset(){
     $('form')[0].reset();
@@ -57,8 +60,8 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.chat-main__message-lists').append(html);
-      $('.chat-main__message-lists').animate({ scrollTop: $('.chat-main__message-lists')[0].scrollHeight });
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
@@ -69,7 +72,7 @@ $(function(){
   })
 
   var reloadMessages = function() {
-    var last_message_id = $('.chat-main__message-lists__message:last').data("message-id");
+    var last_message_id = $('.message:last').data("message-id");
     $.ajax({
       url: "api/messages",
       type: 'GET',
@@ -82,8 +85,8 @@ $(function(){
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        $('.chat-main__message-lists').append(insertHTML);
-        $('.chat-main__message-lists').animate({ scrollTop: $('.chat-main__message-lists')[0].scrollHeight });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
       }
     })
     .fail(function() {
